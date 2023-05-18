f07ad6fb90c2bc47e2603aaf1760d6aab31eb4d9
Md Arifur Rahman <44564356+amialif@users.noreply.github.com>
2023-05-18 23:48:24 +0800

    main
    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import firebase from "./FirebaseConfig";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Firebase Recipies</h2>
+    </div>
+  );
+}
+
+export default App;
